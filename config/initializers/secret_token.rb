b70b8af5f77e352092c772908e1bc7612c44df99
Greg Schafer <grschafer@gmail.com>
2013-09-01 15:09:59 -0500

    Add Rails new site skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Dota2rails::Application.config.secret_key_base = '67082e8ec3e9cb8154057010dc91eefd354c2483556bff0fa95ceecb11ed72a7ead711d78b59b4a186bc47c8056561c52c0dad44544ceb15a494de0dde94eee4'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Dota2rails::Application.config.session_store :cookie_store, key: '_dota2rails_session'
