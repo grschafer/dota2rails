67a247502d55d6057860229d1d8bab42a35734ce
Greg Schafer <grschafer@gmail.com>
2013-09-30 19:35:52 -0500

    Merge gold/xp data into single graph

--- app/assets/javascripts/matches/goldxp.js
+++ app/assets/javascripts/matches/goldxp.js
@@ -1,70 +1,68 @@
 window.DOTA2RAILS.matches.goldxp = function() {
   var nvChart;
-  var elems = ['#gold_chart', '#xp_chart'];
-  [0,1].forEach(function (i) {
-    nv.addGraph({
-      generate: function() {
-        var id = elems[i];
-        var chart = nv.models.lineChart();
+  nv.addGraph({
+    generate: function() {
+      var id = '#chart';
+      var chart = nv.models.lineChart();
 
-        chart.xAxis
-          .axisLabel('Time (ms)')
-          .tickFormat(d3.format(',r'));
+      chart.xAxis
+        .axisLabel('Time (ms)')
+        .tickFormat(d3.format(',r'));
 
-        chart.yAxis
-          .axisLabel('Voltage (v)')
-          .tickFormat(d3.format('.02f'));
+      chart.yAxis
+        .axisLabel('Voltage (v)')
+        .tickFormat(d3.format('.02f'));
 
-        d3.select(id + ' svg')
-          .datum(gold_xp_data(id))
-        .transition().duration(500)
-          .call(chart);
+      d3.select(id + ' svg')
+        .datum(gold_xp_data(id))
+      .transition().duration(500)
+        .call(chart);
 
-        nv.utils.windowResize(function() { d3.select(id + ' svg').call(chart) });
+      nv.utils.windowResize(function() { d3.select(id + ' svg').call(chart) });
 
-        nvChart = chart;
-        return chart;
-      },
-      callback: function(graph) {
-        line_home = d3.selectAll('.nv-x.nv-axis .nvd3').append('g');
-        line_home.attr('opacity', '1');
-        line_home.each(function (d,i) {
-          var max = $(this).siblings('.nv-axisMaxMin').last().children().prop('__data__');
-          var t = $(this).siblings('.nv-axisMaxMin').last().attr('transform');
-          var width = parseFloat(t.substring(t.indexOf("(") + 1, t.indexOf(",")));
-          var min = $(this).siblings('.nv-axisMaxMin').first().children().prop('__data__');
-          var scale = width / (max - min);
-          var offset = -min * scale;
-          line_home.attr("transform", "translate(" + offset + " 0) scale(" + scale + " 1)")
-          line_home.style("stroke-width", 1/scale);
-        });
-      }
-    });
+      nvChart = chart;
+      return chart;
+    },
+    callback: function(graph) {
+      line_home = d3.selectAll('.nv-x.nv-axis .nvd3').append('g');
+      line_home.attr('opacity', '1');
+      line_home.each(function (d,i) {
+        var max = $(this).siblings('.nv-axisMaxMin').last().children().prop('__data__');
+        var t = $(this).siblings('.nv-axisMaxMin').last().attr('transform');
+        var width = parseFloat(t.substring(t.indexOf("(") + 1, t.indexOf(",")));
+        var min = $(this).siblings('.nv-axisMaxMin').first().children().prop('__data__');
+        var scale = width / (max - min);
+        var offset = -min * scale;
+        line_home.attr("transform", "translate(" + offset + " 0) scale(" + scale + " 1)")
+        line_home.style("stroke-width", 1/scale);
+      });
+    }
   });
 
   function gold_xp_data(id) {
     var colors = ["rgba(39, 105, 229, 1)", "rgba(92, 229, 172, 1)", "rgba(172, 0, 172, 1)", "rgba(219, 216, 9, 1)", "rgba(229, 97, 0, 1)", "rgba(229, 121, 175, 1)", "rgba(145, 163, 63, 1)", "rgba(91, 196, 223, 1)", "rgba(0, 118, 30, 1)", "rgba(148, 95, 0, 1)"];
-    var data_label = id.substring(1, id.indexOf("_"));
-    var data = $(id).data(data_label);
     var dataset = [];
-    var values = [];
-    for (var i = 0; i < data.tick.length; i++) {
-      // TODO: get which team heroes are on!
-      var c = 0;
-      var r = 0;
-      var d = 0;
-      for (var hero in data) {
-        if (hero === 'tick')
-          continue
-        if (c < 5)
-          r += (data[hero][i] !== undefined) ? data[hero][i] : data[hero][data[hero].length - 1]
-        else
-          d += (data[hero][i] !== undefined) ? data[hero][i] : data[hero][data[hero].length - 1]
-        c++
+    for (var data_label in {gold:0, xp:0}) {
+      var data = $(id).data(data_label);
+      var values = [];
+      for (var i = 0; i < data.tick.length; i++) {
+        // TODO: get which team heroes are on!
+        var c = 0;
+        var r = 0;
+        var d = 0;
+        for (var hero in data) {
+          if (hero === 'tick')
+            continue
+          if (c < 5)
+            r += (data[hero][i] !== undefined) ? data[hero][i] : data[hero][data[hero].length - 1]
+          else
+            d += (data[hero][i] !== undefined) ? data[hero][i] : data[hero][data[hero].length - 1]
+          c++
+        }
+        values.push({'x': data.tick[i], 'y': r - d});
       }
-      values.push({'x': data.tick[i], 'y': r - d});
+      dataset.push({key: data_label, color: (data_label === 'gold') ? 'blue' : 'green', values: values});
     }
-    dataset.push({key: data_label, color: 'black', values: values});
     return dataset;
   }
 
